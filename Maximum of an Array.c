da1a14245646b14e7abbd77c32dc2ea6ea6c71db
lokeshyerubandi <yerubandilokeshsaibalaji@gmail.com>
2024-11-14 21:37:40 +0530

    Uploading code: Maximum of an Array.c

---
+++ Maximum of an Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int A[n];
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    int max=A[0];
+    for (int i=0;i<n;i++)
+    {
+        if (A[i]>max)
+        {
+            max=A[i];
+        }
+    }
+    printf("%d",max);
+}
